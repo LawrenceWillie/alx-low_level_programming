224f93c4620a87d8a455856293bfc166c4191ecb
LawrenceWillie <lawrencewillie448@gmail.com>
2023-09-26 06:22:54 -0700

    message

--- 0x0A-argc_argv/4-add.c
+++ 0x0A-argc_argv/4-add.c
@@ -11,10 +11,8 @@
 int check_num(char *str)
 {
 /*Declaring variables*/
-
 unsigned int count;
 count = 0;
-
 while (count < strlen(str)) /*count string*/
 {
 if (!isdigit(str[count])) /*check if str there are digit*/
@@ -34,13 +32,11 @@ return (1);
 */
 int main(int argc, char *argv[])
 {
-
 /*Declaring variables*/
 int count;
 int str_to_int;
 int sum = 0;
 count = 1;
-
 while (count < argc) /*Goes through the whole array*/
 {
 if (check_num(argv[count]))
